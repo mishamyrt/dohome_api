4afdb2dd57c60ec00ee915f409ada4c5bafd8b05
mishamyrt <misha@myrt.co>
2022-10-30 02:28:51 +0300

    Add light by sid only

--- dohome/gateway/__init__.py
+++ dohome/gateway/__init__.py
@@ -2,12 +2,15 @@
 from socket import getfqdn, gethostname, gethostbyname_ex
 from ..constants import API_PORT
 from ..datagram import open_broadcast
+from ..light import DoHomeLight
 from ..commands import (
-    REQUEST_PING
+    REQUEST_PING,
+    CMD_GET_IP,
+    format_request,
+    parse_response
 )
 from .util import (apply_mask, parse_pong)
 
-# 'cmd=ctrl&devices=[\'7b5b\']&op={"cmd":19}'
 class DoHomeGateway:
     """DoHome gateway controller"""
 
@@ -18,16 +21,36 @@ class DoHomeGateway:
         self._host = host if host is not None else self._get_discovery_host()
         self._broadcast = None
 
-    async def discover_lights(self, timeout=1.0):
+    async def add_light(self, sid: str) -> DoHomeLight:
+        host = await self.discover_ip(sid)
+        print(host)
+        return DoHomeLight(sid, host)
+
+    async def discover_ip(self, sid: str) -> str:
+        """Discovers DoHome light IP"""
         await self._connection()
-        self._broadcast.send(REQUEST_PING)
-        responses = await self._broadcast.receive(timeout)
+        responses = await self._request(
+            format_request([sid], CMD_GET_IP)
+        )
+        if len(responses) != 1:
+            raise IOError
+        parts = responses[0].decode("utf-8").split('"')
+        return parts[len(parts) - 2]
+
+    async def discover_lights(self, timeout=1.0):
+        """Discovers DoHome lights"""
+        await self._connection()
+        responses = await self._request(REQUEST_PING, timeout)
         descriptions = []
         for response in responses:
             message = response.decode("utf-8")
             if message.startswith('cmd=pong'):
                 descriptions.append(parse_pong(message))
         return descriptions
+
+    async def _request(self, req: str, timeout=0.2) -> list:
+        self._broadcast.send(req.encode())
+        return await self._broadcast.receive(timeout)
 
     async def _connection(self):
         if self._broadcast is None or self._broadcast.closed:
--- dohome/light.py
+++ dohome/light.py
@@ -15,7 +15,8 @@ from .commands import (
     CMD_GET_TIME,
     CMD_SET_POWER,
     format_request,
-    format_light_request
+    format_light_request,
+    parse_response
 )
 from .constants import (
     API_PORT,
@@ -42,15 +43,6 @@ class DoHomeLight():
     def connected(self):
         """Indicates whether the socket is connected."""
         return self._conn is not None and not self._conn.closed
-
-    async def connect(self):
-        """Create socket to light"""
-        if self.connected:
-            self._conn.close()
-        self._conn = await open_endpoint(
-            self.HOST,
-            API_PORT
-        )
 
     async def get_state(self) -> dict:
         """Reads high-level state from the device"""
@@ -83,19 +75,19 @@ class DoHomeLight():
     async def get_raw_state(self):
         """Reads color from the device"""
         return await self._send_request(
-            format_request(self.SID, CMD_GET_STATE)
+            format_request([self.SID], CMD_GET_STATE)
         )
 
     async def get_time(self):
         """Reads time from the device"""
         await self._send_request(
-            format_request(self.SID, CMD_GET_TIME)
+            format_request([self.SID], CMD_GET_TIME)
         )
 
     async def set_enabled(self, enabled: bool):
         """Turns the device off"""
         return await self._send_request(
-            format_request(self.SID, CMD_SET_POWER, { "op": 1 if enabled else 0 })
+            format_request([self.SID], CMD_SET_POWER, { "op": 1 if enabled else 0 })
         )
 
     async def set_white(self, mireds: int, brightness = 255):
@@ -104,7 +96,7 @@ class DoHomeLight():
         warm_white = 5000 * white_percent
         return await self._send_request(
             format_light_request(
-                self.SID,
+                [self.SID],
                 w=apply_brightness(5000 - warm_white, brightness),
                 m=apply_brightness(warm_white, brightness)
             )
@@ -115,22 +107,30 @@ class DoHomeLight():
         """Sets RGB color to the device"""
         return await self._send_request(
             format_light_request(
-                self.SID,
+                [self.SID],
                 apply_brightness(uint8_to_dohome(r), brightness),
                 apply_brightness(uint8_to_dohome(g), brightness),
                 apply_brightness(uint8_to_dohome(b), brightness)
             )
         )
 
+    async def _connect(self):
+        """Create socket to light"""
+        if self.connected:
+            self._conn.close()
+        self._conn = await open_endpoint(
+            self.HOST,
+            API_PORT
+        )
+
     async def _send_request(self, request: str):
+        if self._conn is None or self._conn.closed:
+            await self._connect()
         _LOGGER.debug("Sending request: %s to %s", request, self.HOST)
         self._conn.send(request.encode())
         response_data = await self._conn.receive()
-        response = response_data.decode("utf-8")
-        parts = response.split('&')
-        json_data = parts[len(parts) - 1].split('=')[1].strip()
-        data = loads(json_data)
-        if data['res'] != 0:
+        response = parse_response(response_data.decode("utf-8"))
+        if response["res"] != 0:
             raise Exception('Command error')
-        return data
+        return response
     
