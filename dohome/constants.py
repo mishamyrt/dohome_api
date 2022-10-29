7c71525bd09e6be00d72c69c65a9db5f55ae0486
mishamyrt <misha@myrt.co>
2022-10-29 23:59:00 +0300

    Clean up

--- dohome/constants.py
+++ dohome/constants.py
@@ -1,12 +1,5 @@
-"""DoHome contantants"""
+"""DoHome API contantants"""
 from typing import Final
-
-# Commands
-CMD_SET_STATE: Final = 6
-CMD_GET_STATE: Final = 25
-CMD_SET_POWER: Final = 5
-CMD_GET_TIME: Final = 9
-CMD_PING: Final = 'cmd=ping\r\n'.encode()
 
 # Connection
 API_PORT: Final = 6091
